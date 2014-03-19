c84867f8c4f34fda27c4d4ac329aa15d1ea26c2a
xiaobo <xiaobosheng@outlook.com>
2014-03-18 22:03:20 -0400

    update fibonacci

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,15 +1,22 @@
-# -*- coding: cp1252 -*-
+# -*- coding: utf-8 -*-
 # Fibonacci Sequence - Enter a number and have the
 # program generate the Fibonacci sequence to that number
 # or to the Nth number
 
-n = int(raw_input('How many numbers do you need? '))
-series = [1]
+def memoize(f):
+    cache={}
+    def decorated_function(*args):
+        if args in cache: 
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorated_function
 
-while len(series) < n:
-    if len(series) == 1:
-        series.append(1)
-    else:
-        series.append(series[-1] + series[-2])
+@memoize
+def fib(n):
+    if n<2: return n
+    return fib(n-2) + fib(n-1)
 
-print series
+
+print fib(100)
