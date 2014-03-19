482a2e734e692cf54874c3f7839069beb0f14416
xiaobo <xiaobosheng@outlook.com>
2014-03-18 21:48:01 -0400

    Numbers pi

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,10 +1,10 @@
 # Find PI to the Nth Digit
+# -*- coding: utf-8 -*-
 
-from math import *
+from math import atan
+from types import IntType
+def pidecimal(n):
+    assert type(n) is IntType, "number is not an interger %r" %n
+    print ('{:.%df}' % n).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
 
-digits = raw_input('Enter number of digits to round PI to: ')
-
-# print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string formatting
-
-# calculate pi using Machin-like Formula
-print ('{0:.%df}' % min(30, int(digits))).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
+pidecimal(10)
